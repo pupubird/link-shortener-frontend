df8099b450ea74a0e5207d3856bfbc322c3c93df
pupubird <rainchai4240@gmail.com>
2021-09-19 18:59:00 +0800

    Create shorten UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,71 @@
 <script>
-	export let name;
+  let domain = "<YOUR DOMAIN>";
+  let output;
+  let name, link;
+  async function shortenLink() {
+    let res = await fetch(`${domain}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        url: link,
+      }),
+    });
+    if (res.ok) {
+      output = domain + "/" + name;
+    } else {
+      alert("Please try again with another name");
+    }
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<form
+  name="shortenForm"
+  class="shortenForm"
+  on:click|preventDefault={shortenLink}
+>
+  <input
+    type="text"
+    name="name"
+    placeholder="Name of the shortened link"
+    bind:value={name}
+    class="shortenForm__name"
+  />
+  <input
+    type="text"
+    name="link"
+    placeholder="Link you want to shorten"
+    bind:value={link}
+    class="shortenForm__link"
+  />
+  <button>Make it shorter!</button>
+  {#if output}
+    <br />
+    <p>Generated link:</p>
+    <a href={output} target="_blank">{output}</a>
+  {/if}
+</form>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .shortenForm {
+    width: clamp(200px, 500px, 95vw);
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #e0e0e0;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  .shortenForm__name {
+    display: block;
+    width: 100%;
+  }
+  .shortenForm__link {
+    display: block;
+    width: 100%;
+  }
 </style>
